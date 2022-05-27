4f7ec5b02e0ec680f463c5906fe2cebf177baab4
Myohres <77498044+Myohres@users.noreply.github.com>
2022-05-27 22:45:02 +0200

    day calculator start

---
+++ src/data/Pause.java
@@ -0,0 +1,14 @@
+package data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Calendar;
+
+@Getter
+@Setter
+public class Pause {
+
+    Calendar hourPauseStart;
+    Calendar hourPauseEnd;
+}
